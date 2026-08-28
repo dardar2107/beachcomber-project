45fec0823cd15c9941a5b6f7b5dd1374764e8ea3
Darwin Ragoonaden <darwinr@Darwins-MacBook-Pro.local>
2026-08-28 14:15:47 +0400

    Initial Vite scaffold

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+import '../css/main.css';
+
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Lenis from 'lenis';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const lenis = new Lenis({
+  autoRaf: false,
+});
+
+lenis.on('scroll', ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
